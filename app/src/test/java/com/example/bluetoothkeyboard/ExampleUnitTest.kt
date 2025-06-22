aa7b60ba3a7f73e9531852f27128ef5226016b95
election result <election.result.official@gmail.com>
2025-06-22 11:55:56 +0530

    Add HidKeyMap usage ID unit tests

--- app/src/test/java/com/example/bluetoothkeyboard/ExampleUnitTest.kt
+++ app/src/test/java/com/example/bluetoothkeyboard/ExampleUnitTest.kt
@@ -14,4 +14,11 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun hidKeyMap_returnsCorrectCodes() {
+        assertEquals(0x04, HidKeyMap.getUsageId('a'))
+        assertEquals(0x05, HidKeyMap.getUsageId('B'))
+        assertEquals(0x2C, HidKeyMap.getUsageId(' '))
+    }
 }
